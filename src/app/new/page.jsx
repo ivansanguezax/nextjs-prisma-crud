7df6d79c445a351b7707f97c71c3ea1facc472fb
ivansanguezax <ivansanguezax@gmail.com>
2023-11-23 14:48:59 -0400

    [004-Nextjs-RenderTask]

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+'use client'
+import { useRouter } from "next/navigation";
+
 
 function NewPage() {
+
+  const router = useRouter();
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const title = event.target.title.value
+    const description = event.target.description.value
+    const res = await fetch('/api/task', {
+      method: 'POST',
+      body: JSON.stringify({ title, description }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    const data = await res.json();
+    console.log(data);
+    router.push('/')
+  }
+
   return (
-    <div>
-      Formulario de tareas
+    <div className="bg-black h-screen flex items-center justify-center">
+      <form onSubmit={onSubmit} className="bg-gray-800 p-8 rounded-md shadow-md text-white">
+        <div className="mb-4">
+          <label htmlFor="title" className="block text-sm font-semibold mb-2">Título:</label>
+          <input
+            className="w-full p-2 border border-gray-600 rounded-md bg-gray-900 focus:outline-none focus:border-blue-500 text-white"
+            type="text"
+            id="title"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="description" className="block text-sm font-semibold mb-2">Contenido:</label>
+          <textarea
+            className="w-full p-2 border border-gray-600 rounded-md bg-gray-900 focus:outline-none focus:border-blue-500 text-white"
+            rows="3"
+            id="description"
+          ></textarea>
+        </div>
+        <button
+          className="bg-blue-700 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue"
+          type="submit"
+        >
+          Crear
+        </button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default NewPage
+export default NewPage;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,24 @@
+import { prisma } from "@/libs/prisma";
 
-function HomePage() {
-  return (
-    <div>
-      Tareas
-    </div>
-  )
+async function loadTask() {
+  return await prisma.task.findMany();
 }
 
-export default HomePage
+async function HomePage() {
+  const tasks = await loadTask();
+  console.log(tasks);
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Tasks</h1>
+      {tasks.map((task) => (
+        <div key={task.id} className="bg-gray-800 p-6 mb-4 rounded-md shadow-md text-white hover:bg-gray-700 transition duration-300 ease-in-out">
+          <h2 className="text-xl font-bold mb-2">{task.title}</h2>
+          <p className="text-gray-300 mb-2">{task.description}</p>
+          <p className="text-gray-500 text-sm">Created at: {new Date (task.createdAt).toLocaleDateString()}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default HomePage;
